9328e9058f072e18c8be0bb0fd32c8ffc424809a
andrea-s21 <andreasisneros21@gmail.com>
2022-03-10 10:06:22 -0700

    deploy

--- routes/api/books.js
+++ routes/api/books.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const ensureLoggedIn = require('../../config/ensureLoggedIn')
 const booksCtrl = require('../../controllers/api/books');
 
-router.use(ensureLoggedIn);
+
 
 router.post('/search', booksCtrl.search);
 router.post('/add', booksCtrl.addBook);
 router.get('/', booksCtrl.index);
-router.get('/listings', booksCtrl.getUserListings);
+router.get('/listings', ensureLoggedIn, booksCtrl.getUserListings);
 
 module.exports = router;
